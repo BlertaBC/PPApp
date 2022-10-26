18a04c9c6b8d54e3e795fdfe58fff13cd10f7c01
Toska30 <blertatoska30@gmail.com>
2022-10-26 15:35:38 +0200

    test2

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 
 
-/*Display information of desired product with react native and axios. This is the url : https://www.ahlsell.se/products/varme--sanitet/pumpar/cirkulation/cirkulationspumpar-for-tappvarmvatten/5805964/*/
+/*Display information of desired product withaxios. This is the url : https://www.ahlsell.se/products/varme--sanitet/pumpar/cirkulation/cirkulationspumpar-for-tappvarmvatten/5805964/*/
 
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Linking } from 'react-native';
 import axios from 'axios';
 
@@ -77,7 +77,90 @@ const styles = StyleSheet.create({
     height: 300,
     alignSelf: 'center',
   },
+}); */
+
+
+/*get the list of information from this product with respective url: https://www.ahlsell.se/products/varme--sanitet/pumpar/cirkulation/cirkulationspumpar-for-tappvarmvatten/5805964/
+use hooks!!!*/  
+
+
+
+/* We have this product information website and we want to retrive information from that website in our app. The url are stored in a Database and we will retrive infromation with axios.
+URL: https://www.ahlsell.se/products/varme--sanitet/pumpar/cirkulation/cirkulationspumpar-for-tappvarmvatten/5805964/*/
+
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Linking } from 'react-native';
+import axios from 'axios';
+
+export default function App() {
+  const [product, setProduct] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://www.ahlsell.se/products/varme--sanitet/pumpar/cirkulation/cirkulationspumpar-for-tappvarmvatten/5805964/')
+
+      .then((response) => {
+        setProduct(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+
+        <Text style={styles.text}>Loading...</Text>
+
+      </View>
+
+    );
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.text}>{product.name}</Text>
+      <Text style={styles.text}>{product.price}</Text>
+      <Text style={styles.text}>{product.description}</Text>
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <TouchableOpacity onPress={() => Linking.openURL(product.url)}>
+
+        <Text style={styles.text}>Go to product</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+  },
 });
+
+  
+
+
+  
+
+
+
 
 
 
@@ -112,3 +195,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 }); */
+
+
